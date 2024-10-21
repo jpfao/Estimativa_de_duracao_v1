6e921edb3e060910405cca539f7542f47b3a051e
jpfao <153869665+jpfao@users.noreply.github.com>
2024-10-20 21:36:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,18 @@ import os
 import streamlit as st
 import pandas as pd
 
-# Function to load the Excel sheet
+# Função para carregar a planilha Excel
 @st.cache_data
 def load_data():
     file_path = 'NovaBasePRF_2021-2024_Codificados&Dinamica_06_09_outliers.xlsx'
     if not os.path.isfile(file_path):
-        st.error(f"File not found: {file_path}")
-        return pd.DataFrame()  # Return an empty DataFrame or handle the error appropriately
-    return pd.read_excel(file_path)
-
+        st.error(f"Arquivo não encontrado: {file_path}")
+        return pd.DataFrame()  # Retorna um DataFrame vazio ou trata o erro apropriadamente
+    try:
+        return pd.read_excel(file_path)
+    except KeyError as e:
+        st.error(f"Erro ao carregar o arquivo: {e}")
+        return pd.DataFrame()
 
 # Função para filtrar os valores das listas de acordo com a seleção
 def filter_options(df, atividade, operacao, etapa):
@@ -22,24 +25,27 @@ def filter_options(df, atividade, operacao, etapa):
 # Carregar os dados da planilha Excel
 df = load_data()
 
-# Listas de valores únicos para picklists
-atividades = df['ATIVIDADE'].unique()
-operacoes = df['OPERACAO'].unique()
-etapas = df['ETAPA'].unique()
+if not df.empty:
+    # Listas de valores únicos para picklists
+    atividades = df['ATIVIDADE'].unique()
+    operacoes = df['OPERACAO'].unique()
+    etapas = df['ETAPA'].unique()
 
-# Interatividade com o usuário - Picklists
-st.title('Formulário Interativo para Sequência Operacional')
+    # Interatividade com o usuário - Picklists
+    st.title('Formulário Interativo para Sequência Operacional')
 
-atividade_selecionada = st.selectbox('Selecione a ATIVIDADE:', atividades)
-operacao_selecionada = st.selectbox('Selecione a OPERACAO:', operacoes)
-etapa_selecionada = st.selectbox('Selecione a ETAPA:', etapas)
+    atividade_selecionada = st.selectbox('Selecione a ATIVIDADE:', atividades)
+    operacao_selecionada = st.selectbox('Selecione a OPERACAO:', operacoes)
+    etapa_selecionada = st.selectbox('Selecione a ETAPA:', etapas)
 
-# Inputs numéricos
-fase = st.number_input('FASE:', min_value=0, max_value=100)
-extensao = st.number_input('EXTENSÃO:', min_value=0.0)
+    # Inputs numéricos
+    fase = st.number_input('FASE:', min_value=0, max_value=100)
+    extensao = st.number_input('EXTENSÃO:', min_value=0.0)
 
-# Filtrando dados
-df_filtrado = filter_options(df, atividade_selecionada, operacao_selecionada, etapa_selecionada)
+    # Filtrando dados
+    df_filtrado = filter_options(df, atividade_selecionada, operacao_selecionada, etapa_selecionada)
 
-st.write('Amostragem dos dados correspondentes:')
-st.write(df_filtrado)
+    st.write('Amostragem dos dados correspondentes:')
+    st.write(df_filtrado)
+else:
+    st.warning("Nenhum dado disponível para exibição.")

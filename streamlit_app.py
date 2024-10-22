e0598ffaf62fb28fd0cf957bc21cf135bb52a696
jpfao <153869665+jpfao@users.noreply.github.com>
2024-10-21 22:53:01 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ if uploaded_file is not None:
             # Criar faixa amarela com menor espessura e texto ajustado
             st.markdown(f"""
                 <div style='background-color: #F0F2F6; padding: 1px; margin-bottom: 10px;'>
-                    <h4 style='color: white; text-align: left; margin: 5; font-size: 110%;'>Linha {row + 1}</h4>
+                    <h4 style='color: black; text-align: left; margin: 5; font-size: 110%;'>Linha {row + 1}</h4>
                 </div>
             """, unsafe_allow_html=True)
 

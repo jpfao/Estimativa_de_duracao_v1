609b0d8941d5d280ad6d517b1adca9a924d3311e
jpfao <153869665+jpfao@users.noreply.github.com>
2024-10-26 00:57:54 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,14 +39,64 @@ if uploaded_file is not None:
         # Exibir o número de linhas e colunas do arquivo
         st.success(f"Arquivo carregado com sucesso! Tamanho: {df.shape[0]} linhas e {df.shape[1]} colunas.")
         
-        # Filtrar os dados entre Outliers e não-Outliers
-        df_non_outliers = df[df['Outlier'] == False]
-        df_outliers = df[df['Outlier'] == True]
-        
+        st.title('Formulário Interativo para Sequência Operacional')
+
+        # Renderizar formulários interativos
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            atividade = st.selectbox('ATIVIDADE:', ['Todos'] + df['ATIVIDADE'].unique().tolist())
+            if atividade == 'Todos':
+                atividade = None
+
+        with col2:
+            operacao = st.selectbox('OPERAÇÃO:', ['Todos'] + df['OPERACAO'].unique().tolist())
+            if operacao == 'Todos':
+                operacao = None
+
+        with col3:
+            etapa = st.selectbox('ETAPA:', ['Todos'] + df['ETAPA'].unique().tolist())
+            if etapa == 'Todos':
+                etapa = None
+
+        with col4:
+            fase = st.selectbox('FASE:', ['Todos'] + df['FASE'].unique().tolist())
+            if fase == 'Todos':
+                fase = None
+
+        col5, col6, col7, col8 = st.columns(4)
+
+        with col5:
+            obz = st.selectbox('OBZ:', ['Todos'] + df['Obz'].unique().tolist())
+            if obz == 'Todos':
+                obz = None
+
+        with col6:
+            broca = st.multiselect('DIÂMETRO BROCA:', ['Todos'] + df['Diâmetro Broca'].unique().tolist(), default='Todos')
+            if 'Todos' in broca:
+                broca = None
+
+        with col7:
+            revestimento = st.multiselect('DIÂMETRO REVESTIMENTO:', ['Todos'] + df['Diâmetro Revestimento'].unique().tolist(), default='Todos')
+            if 'Todos' in revestimento:
+                revestimento = None
+
+        with col8:
+            tipo_sonda = st.multiselect('TIPO SONDA:', ['Todos'] + df['Tipo_sonda'].unique().tolist(), default='Todos')
+            if 'Todos' in tipo_sonda:
+                tipo_sonda = None
+
+        # Aplicar filtro com base nos valores selecionados
+        df_filtered = filter_options(df, atividade=atividade, operacao=operacao, etapa=etapa, fase=fase, obz=obz, broca=broca, revestimento=revestimento, tipo_sonda=tipo_sonda)
+
+        # Dividir os dados filtrados em Outliers e Não-Outliers
+        df_non_outliers = df_filtered[df_filtered['Outlier'] == False]
+        df_outliers = df_filtered[df_filtered['Outlier'] == True]
+
         # Exibir tabela de amostras onde 'Outlier' é False
         st.write("Tabela - Amostras sem Outliers (Outlier = False):")
         st.dataframe(df_non_outliers.reset_index(drop=True))
-        
+
         # Exibir tabela de amostras onde 'Outlier' é True
         st.write("Tabela - Amostras com Outliers (Outlier = True):")
         st.dataframe(df_outliers.reset_index(drop=True))

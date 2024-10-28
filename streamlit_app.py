6f67d4cf493d4528843ccc2e6b56b55aa73ab1ed
jpfao <153869665+jpfao@users.noreply.github.com>
2024-10-28 02:50:27 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Função para filtrar as opções e remover outliers
 @st.cache_data
@@ -24,6 +26,24 @@ def filter_options(df, atividade=None, operacao=None, etapa=None, fase=None, obz
         df_filtered = df_filtered[df_filtered['Tipo_sonda'].isin(tipo_sonda)]
     
     return df_filtered
+
+# Função para gerar o boxplot com rótulos
+def plot_boxplot_with_labels(data, column, lim_inf, lim_sup, title):
+    plt.figure(figsize=(12, 6))
+    sns.boxplot(x=data[column], color='lightblue', flierprops={'marker': 'o', 'color': 'red'})
+    median_value = data[column].median()
+    
+    # Adicionar rótulos para a mediana, limite inferior e limite superior
+    plt.axvline(median_value, color='green', linestyle='--', label=f'Mediana: {median_value:.2f}')
+    plt.axvline(lim_inf, color='blue', linestyle='--', label=f'Limite Inferior: {lim_inf:.2f}')
+    plt.axvline(lim_sup, color='orange', linestyle='--', label=f'Limite Superior: {lim_sup:.2f}')
+    
+    plt.title(title)
+    plt.xlabel(column)
+    plt.legend()
+    plt.grid()
+    st.pyplot(plt.gcf())  # Mostrar o gráfico no Streamlit
+    plt.close()
 
 # Definir a largura da página como ampla
 st.set_page_config(layout="wide")
@@ -70,81 +90,27 @@ if uploaded_file is not None:
                     broca = [row.get('Diâmetro Broca')] if pd.notna(row.get('Diâmetro Broca')) else None
                     revestimento = [row.get('Diâmetro Revestimento')] if pd.notna(row.get('Diâmetro Revestimento')) else None
 
-                    # Renderizar campos de seleção de filtros
-                    col1, col2, col3, col4 = st.columns(4)
-
-                    with col1:
-                        atividade = st.selectbox(f'ATIVIDADE (Linha {i + 1}, {avancado}):', ['Todos'] + df['ATIVIDADE'].unique().tolist(), 
-                                                 index=(df['ATIVIDADE'].unique().tolist().index(atividade) + 1) if atividade and atividade in df['ATIVIDADE'].unique() else 0)
-                        if atividade == 'Todos':
-                            atividade = None
-
-                    with col2:
-                        operacao = st.selectbox(f'OPERAÇÃO (Linha {i + 1}, {avancado}):', ['Todos'] + df['OPERACAO'].unique().tolist(), 
-                                                index=(df['OPERACAO'].unique().tolist().index(operacao) + 1) if operacao and operacao in df['OPERACAO'].unique() else 0)
-                        if operacao == 'Todos':
-                            operacao = None
-
-                    with col3:
-                        etapa = st.selectbox(f'ETAPA (Linha {i + 1}, {avancado}):', ['Todos'] + df['ETAPA'].unique().tolist(), 
-                                             index=(df['ETAPA'].unique().tolist().index(etapa) + 1) if etapa and etapa in df['ETAPA'].unique() else 0)
-                        if etapa == 'Todos':
-                            etapa = None
-
-                    with col4:
-                        fase = st.selectbox(f'FASE (Linha {i + 1}, {avancado}):', ['Todos'] + df['FASE'].unique().tolist(), 
-                                            index=(df['FASE'].unique().tolist().index(fase) + 1) if fase and fase in df['FASE'].unique() else 0)
-                        if fase == 'Todos':
-                            fase = None
-
-                    col5, col6, col7, col8 = st.columns(4)
-
-                    with col5:
-                        obz = st.selectbox(f'COM AVANÇO/SEM AVANÇO (Linha {i + 1}, {avancado}):', ['Todos'] + df['Obz'].unique().tolist(), 
-                                           index=(df['Obz'].unique().tolist().index(avancado) + 1) if avancado and avancado in df['Obz'].unique() else 0)
-                        if obz == 'Todos':
-                            obz = None
-
-                    with col6:
-                        broca = st.multiselect(f'DIÂMETRO BROCA (Linha {i + 1}, {avancado}):', ['Todos'] + df['Diâmetro Broca'].unique().tolist(), default=broca or ['Todos'])
-                        if 'Todos' in broca:
-                            broca = None
-
-                    with col7:
-                        revestimento = st.multiselect(f'DIÂMETRO REVESTIMENTO (Linha {i + 1}, {avancado}):', ['Todos'] + df['Diâmetro Revestimento'].unique().tolist(), default=revestimento or ['Todos'])
-                        if 'Todos' in revestimento:
-                            revestimento = None
-
-                    with col8:
-                        tipo_sonda = st.multiselect(f'TIPO SONDA (Linha {i + 1}, {avancado}):', ['Todos'] + df['Tipo_sonda'].unique().tolist(), default=tipo_sonda or ['Todos'])
-                        if 'Todos' in tipo_sonda:
-                            tipo_sonda = None
-
                     # Aplicar filtro e exibir os dados
                     df_filtered = filter_options(df, atividade=atividade, operacao=operacao, etapa=etapa, fase=fase, obz=avancado, broca=broca, revestimento=revestimento, tipo_sonda=tipo_sonda)
-                    df_non_outliers = df_filtered[df_filtered['Outlier'] == False]
-                    df_outliers = df_filtered[df_filtered['Outlier'] == True]
+                    
+                    # Remover colunas indesejadas
+                    columns_to_drop = ['depth_range_start', 'depth_range_end', 'Tipo_avanço', 'Extensão']
+                    df_filtered.drop(columns=columns_to_drop, inplace=True, errors='ignore')
 
-                    # Exibir a quantidade de amostras sem e com outliers
-                    st.markdown(
-                        f"<div style='background-color: #E8F4FF; padding: 10px; border-radius: 5px; margin-bottom: 10px; color: #00008B; font-size: 18px; text-align: center;'>"
-                        f"Quantidade de Amostras sem Outliers (Linha {i + 1}, {avancado}): <strong>{df_non_outliers.shape[0]}</strong>"
-                        f"</div>",
-                        unsafe_allow_html=True
-                    )
-                    st.dataframe(df_non_outliers.reset_index(drop=True))
+                    # Exibir o gráfico correspondente para cada grupo
+                    if avancado == "Com avanço" and not df_filtered.empty:
+                        plot_boxplot_with_labels(df_filtered, 'Taxa', df_filtered['LIM_INF_OUT'].min(), df_filtered['LIM_SUP_OUT'].max(),
+                                                 f'Boxplot de Taxa - Linha {i + 1}, Grupo Com avanço')
+                    elif avancado == "Sem avanço" and not df_filtered.empty:
+                        df_filtered.rename(columns={'TEMPO_TOTAL_HORAS': 'Tempo em horas'}, inplace=True)
+                        plot_boxplot_with_labels(df_filtered, 'Tempo em horas', df_filtered['LIM_INF_OUT'].min(), df_filtered['LIM_SUP_OUT'].max(),
+                                                 f'Boxplot de Tempo em horas - Linha {i + 1}, Grupo Sem avanço')
 
-                    st.markdown(
-                        f"<div style='background-color: #FFE8E8; padding: 10px; border-radius: 5px; margin: 20px 0 10px 0; color: #8B0000; font-size: 18px; text-align: center;'>"
-                        f"Quantidade de Amostras com Outliers (Linha {i + 1}, {avancado}): <strong>{df_outliers.shape[0]}</strong>"
-                        f"</div>",
-                        unsafe_allow_html=True
-                    )
-                    st.dataframe(df_outliers.reset_index(drop=True))
+                    # Exibir as amostras filtradas
+                    st.dataframe(df_filtered.reset_index(drop=True))
 
     except Exception as e:
         st.error(f"Ocorreu um erro ao carregar o arquivo: {e}")
 
 else:
     st.warning("Nenhum arquivo foi carregado. Por favor, faça o upload dos arquivos necessários.")
-
